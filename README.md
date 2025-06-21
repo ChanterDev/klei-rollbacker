0bd0c9ca7a9a2130d668a71c453a6134448bb285
ChanterDev <matvey.dibrov@yandex.ru>
2025-06-21 20:33:38 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# How to use
+1. You need [Python 3.11](https://www.python.org/downloads/release/python-3119)
+2. Then change values in "secret.json"
+3. And just run "python main.py" inside your repository
---
+++ main.py
@@ -0,0 +1,46 @@
+from requests import get, post
+from json import load, dump
+
+DATA_URL = "https://accounts.klei.com/account/transactions/data.json"
+ROLLBACK_URL = "https://accounts.klei.com/account/transactions/rollback"
+
+headers_file = {}
+with open("headers.json", 'r', encoding="UTF-8") as file:
+    headers_file = load(file)
+
+secret = {}
+with open("secret.json", 'r', encoding="UTF-8") as file:
+    secret = load(file)
+
+data_headers = headers_file["data"]
+rollback_headers = headers_file["rollback"]
+data_headers["Cookie"] = headers_file["session"] % secret["sessionCookie"]
+rollback_headers["Cookie"] = headers_file["session"] % secret["sessionCookie"]
+del headers_file
+
+transactions = {}
+data_file_response = get(DATA_URL, headers=data_headers)
+del data_headers
+data_file_response.raise_for_status()
+transactions = data_file_response.json()["data"]["Transactions"]
+del data_file_response
+
+
+for transaction in transactions:
+    if transaction["Type"] != "TXN_ITEM_FOR_SPOOLS" or transaction["Time"] < secret["rollbackFrom"]:
+        continue
+    
+    # Если всё прошло гладко
+    rollback_data = {
+        "csrfToken": secret["csrfToken"],
+        "txnID": transaction["TxnID"],
+        "game": secret["game"]
+    }
+    rollback_response = post(ROLLBACK_URL, data=rollback_data, headers=rollback_headers)
+    del rollback_data
+    rollback_response.raise_for_status()
+    secret["csrfToken"] = rollback_response.json()["data"]["NewCSRFToken"]
+    del rollback_response
+
+with open("secret.json", 'w', encoding="UTF-8") as file:
+    dump(secret, file, ensure_ascii=False, indent='\t')
